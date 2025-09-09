b260b1b656b22078e669c778326471313562007c
na9771 <na9771@rit.edu>
2025-09-09 09:58:33 +0400

    Initial

---
+++ variables.py
@@ -0,0 +1,14 @@
+def variables_practice():
+    my_age = 18
+    year = 365 
+    pet_name = "ginger"
+    pi = 3.14
+
+    print("Age: ", my_age)
+    print("Days: ", year)
+    print("Pet: ", pet_name)
+    print("Pi: ", pi)
+
+variables_practice()
+
+
