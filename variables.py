12dd3452b8e8d8f484d5e2cb7c42dec79888f2a5
na9771 <na9771@rit.edu>
2025-09-23 11:01:38 +0400

    Initial

--- variables.py
+++ variables.py
@@ -8,7 +8,51 @@ def variables_practice():
     print("Days: ", year)
     print("Pet: ", pet_name)
     print("Pi: ", pi)
-
 variables_practice()
+
+def prompt_and_print():
+    number1 = float(input("Enter a number: "))
+    number2 = float(input("Enter a number: "))
+    print("Addition: ",number1 + number2)
+    print("Subtraction: ", number1 - number2)
+    print("Multiplication: ", number1 * number2)
+    print("Division: ",number1/number2)
+prompt_and_print()
+
+def circle_area(radius):
+    return 3.14 * radius ** 2
+
+def main():
+    area = circle_area(10) #Function call
+    print(area) #radius = 10
+main()
+
+def triangle_area(base, height):
+    return 1/2 * base * height
+def main():
+    area = triangle_area(20,10)
+    print(area)
+main()
+
+def square_area(length):
+    return length * length
+def main():
+    area = square_area(5)
+    print(area)
+main()
+
+def rectangle_area(length, width):
+    return length * width
+def main():
+    area = rectangle_area(10,15)
+    print(area)
+main()
+
+
+
+
+          
+        
+
 
 
